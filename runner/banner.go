911799ba1bba3334ad33cf6a72f03bf6208b8d86
404tk <uctf@qq.com>
2022-12-08 17:25:38 +0800

    feat(goreleaser):auto build & release

--- runner/banner.go
+++ runner/banner.go
@@ -12,7 +12,7 @@ const banner = `
 `
 
 // Version is the current version of cloudtoolkit
-const Version = `0.0.2`
+const Version = `0.0.3`
 
 // showBanner is used to show the banner to the user
 func ShowBanner() {
