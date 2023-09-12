0da41d11e100ae03a865b9b9cb4cfdea1dd6dffe
404tk <uctf@qq.com>
2023-09-12 18:49:03 +0800

    fix(rds):support list all regions

--- pkg/providers/alibaba/rds/databases.go
+++ pkg/providers/alibaba/rds/databases.go
@@ -2,11 +2,13 @@ package rds
 
 import (
 	"context"
+	"fmt"
 	"math"
 	"strings"
 
 	"github.com/404tk/cloudtoolkit/pkg/schema"
 	"github.com/404tk/cloudtoolkit/utils/logger"
+	"github.com/404tk/cloudtoolkit/utils/processbar"
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk"
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth/credentials"
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
@@ -41,44 +43,84 @@ func (d *Driver) GetDatabases(ctx context.Context) ([]schema.Database, error) {
 	if err != nil {
 		return list, err
 	}
-	page := 1
-	for {
-		describeDBInstancesRequest := rds.CreateDescribeDBInstancesRequest()
-		describeDBInstancesRequest.PageSize = requests.NewInteger(100)
-		describeDBInstancesRequest.PageNumber = requests.NewInteger(page)
-		response, err := client.DescribeDBInstances(describeDBInstancesRequest)
-		if err != nil {
-			logger.Error("Describe database instances failed.")
-			return list, err
-		}
-		pageCount := int(math.Ceil(float64(response.TotalRecordCount) / 100))
-		for _, dbInstance := range response.Items.DBInstance {
-			_db := schema.Database{
-				InstanceId:    dbInstance.DBInstanceId,
-				Engine:        dbInstance.Engine,
-				EngineVersion: dbInstance.EngineVersion,
-				Region:        dbInstance.RegionId,
-				Address:       dbInstance.ConnectionString,
-				NetworkType:   dbInstance.InstanceNetworkType,
-			}
-			// if dbInstance.DBInstanceNetType == "Internet" {
-			// _db.Address = dbInstance.ConnectionString
-			// }
-			_db.DBNames = describeDatabases(client, dbInstance.DBInstanceId)
-			list = append(list, _db)
-		}
-		if page == pageCount || pageCount == 0 {
-			break
-		}
-		page++
-		select {
-		case <-ctx.Done():
+	var regions = []string{d.Region}
+	if d.Region == "all" {
+		regions = describeRegions(client)
+		if len(regions) == 0 {
 			return list, nil
-		default:
-			continue
 		}
 	}
+	flag := false
+	prevLength := 0
+	count := 0
+	for _, r := range regions {
+		page := 1
+		for {
+			request := rds.CreateDescribeDBInstancesRequest()
+			request.PageSize = requests.NewInteger(100)
+			request.PageNumber = requests.NewInteger(page)
+			if r != "all" {
+				request.RegionId = r
+			}
+
+			response, err := client.DescribeDBInstances(request)
+			if err != nil {
+				break
+			}
+			pageCount := int(math.Ceil(float64(response.TotalRecordCount) / 100))
+			for _, dbInstance := range response.Items.DBInstance {
+				_db := schema.Database{
+					InstanceId:    dbInstance.DBInstanceId,
+					Engine:        dbInstance.Engine,
+					EngineVersion: dbInstance.EngineVersion,
+					Region:        dbInstance.RegionId,
+					Address:       dbInstance.ConnectionString,
+					NetworkType:   dbInstance.InstanceNetworkType,
+				}
+				// if dbInstance.DBInstanceNetType == "Internet" {
+				// _db.Address = dbInstance.ConnectionString
+				// }
+				_db.DBNames = describeDatabases(client, dbInstance.DBInstanceId)
+				list = append(list, _db)
+			}
+			if page == pageCount || pageCount == 0 {
+				break
+			}
+			page++
+		}
+		select {
+		case <-ctx.Done():
+			goto done
+		default:
+			prevLength, flag = processbar.RegionPrint(r, len(list)-count, prevLength, flag)
+			count = len(list)
+		}
+	}
+done:
+	if !flag {
+		fmt.Printf("\n\033[F\033[K")
+	}
 	return list, nil
+}
+
+func describeRegions(client *rds.Client) []string {
+	request := rds.CreateDescribeRegionsRequest()
+	request.Scheme = "https"
+	request.AcceptLanguage = "en-US"
+	response, err := client.DescribeRegions(request)
+	if err != nil {
+		logger.Error(err)
+		return []string{}
+	}
+	temp := make(map[string]string)
+	for _, region := range response.Regions.RDSRegion {
+		temp[region.RegionId] = ""
+	}
+	regions := []string{}
+	for region := range temp {
+		regions = append(regions, region)
+	}
+	return regions
 }
 
 func describeDatabases(client *rds.Client, instanceId string) string {
